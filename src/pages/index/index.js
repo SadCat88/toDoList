3d806431d76e388bc2f7dced2c4a06e09661e914
SadCat88 <SadCat88@yandex.ru>
2019-12-23 14:51:35 +0300

    Добавление новых заданий в список.

--- src/pages/index/index.js
+++ src/pages/index/index.js
@@ -17,3 +17,63 @@ import { getAfterContent } from '../../assets/js/getAfterContent.js';
 // =============================================================================
 ('use strict');
 
+let $toDoInput = document.querySelector('.todo-task__input');
+let $toDoButton = document.querySelector('.todo-task__button');
+let $toDoList = document.querySelector('.todo-list');
+
+let taskList = [];
+
+$toDoButton.addEventListener('click', addToDo);
+
+/**
+ * Обрабатывает ввод задачи
+ *
+ * * @deprecated <pre>
+ * Создает объект с новой задачей:
+ * todo: 		  'текст новой задачи',
+ * checked: 	0/1 выполнено,
+ * important: 0/1 важное
+ *
+ * </pre>
+ * @param {object} event - событие
+ */
+function addToDo(event) {
+  event.preventDefault();
+  // отмена действия по умолчанию
+
+  // === новое задание
+  let newToDo = {
+    todo: $toDoInput.value,
+    checked: false,
+    important: false
+  };
+
+  taskList.push(newToDo);
+  // добавить новое задание в массив заданий
+  displayNewToDoIntoList(taskList);
+  // вывести новое задание на экран
+}
+
+/**
+ * Выводит на экран содержимое массива
+ *
+ * @param {arrange} arr - массив содержащий объекты с заданиями и их состоянием
+ */
+function displayNewToDoIntoList(arr) {
+  $toDoList.innerHTML = '';
+  //очистка списка
+
+  // === перебор всех заданий из массива
+  for (let i = 0; i < arr.length; i++) {
+    let $task = document.createElement('li');
+    // создать новый элемент
+    $task.className = 'todo-list__item';
+    // установить для нового элемента класс
+    $task.setAttribute('id', `task_${i}`);
+    // установить для нового элемента атрибут id
+    $task.innerHTML = arr[i].todo;
+    // добавить новому элементу содержание
+    $toDoList.append($task);
+    // добавить новый элемент последним дочерним элементом в список
+  }
+}
